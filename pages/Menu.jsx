1f556b8cdb1b4801e6b5c2745bd0db7353f4b701
obetdelacruz <pazmichaelandrew70@gmail.com>
2023-06-06 07:28:16 +0800

    Added styles for Menu page

--- pages/Menu.jsx
+++ pages/Menu.jsx
@@ -1,26 +1,34 @@
 import { Link } from "react-router-dom";
+import "../styles/menu.css";
 
 function Menu() {
   return (
-    <>
-      <div>Menu</div>
-      <div>
+    <div className="menu-body">
+      <h1 className="title">VISITOR MANAGEMENT SYSTEM</h1>
+      <div className="logo-placement">
+        <div className="company-logo">
+          COMPANY <br />
+          LOGO
+        </div>
+      </div>
+      <section className="menu-options">
         <Link to="/Hello_VMS_FrontEnd/menu/visitor_database">
-          Visitors DataBase
+          <div className="visitor-database">Visitors Database</div>{" "}
+        </Link>
+        <Link to="/Hello_VMS_FrontEnd/menu/monitoring">
+          <div className="monitoring-mode">Monitoring Mode</div>
+        </Link>
+        <Link to="/Hello_VMS_FrontEnd/menu/analytics">
+          <div className="analytics">Analytics</div>
+        </Link>
+      </section>
+      <p className="greeting">Hello Mx. User! Where to today?</p>
+      <div className="logout-btn-placement">
+        <Link to="/Hello_VMS_FrontEnd/">
+          <p className="logout-btn">LOG OUT</p>
         </Link>
       </div>
-      <div>
-        <Link to="/Hello_VMS_FrontEnd/menu/monitoring">Monitoring Mode</Link>
-      </div>
-      <div>
-        <Link to="/Hello_VMS_FrontEnd/menu/analytics">Analytics</Link>
-      </div>
-      <div>
-        <p>
-          <Link to="/Hello_VMS_FrontEnd/">Log-out</Link>
-        </p>
-      </div>
-    </>
+    </div>
   );
 }
 
