75d1133d5dfe6e563f2d78c1260e3fa77a573ba3
obetdelacruz <pazmichaelandrew70@gmail.com>
2023-06-06 08:39:02 +0800

    Added images and finished CSS for deskotp menu

--- pages/Menu.jsx
+++ pages/Menu.jsx
@@ -12,19 +12,27 @@ function Menu() {
         </div>
       </div>
       <section className="menu-options">
-        <Link to="/Hello_VMS_FrontEnd/menu/visitor_database">
-          <div className="visitor-database">Visitors Database</div>{" "}
+        <Link className="links" to="/Hello_VMS_FrontEnd/menu/visitor_database">
+          <div className="visitor-database">
+            VISITOR <br /> DATABASE <br />
+            <img src="./images/database.png" alt="" />
+          </div>{" "}
         </Link>
-        <Link to="/Hello_VMS_FrontEnd/menu/monitoring">
-          <div className="monitoring-mode">Monitoring Mode</div>
+        <Link className="links" to="/Hello_VMS_FrontEnd/menu/monitoring">
+          <div className="monitoring-mode">
+            MONITOR <br /> MODE <br /> <img src="./images/monitor.png" alt="" />
+          </div>
         </Link>
-        <Link to="/Hello_VMS_FrontEnd/menu/analytics">
-          <div className="analytics">Analytics</div>
+        <Link className="links" to="/Hello_VMS_FrontEnd/menu/analytics">
+          <div className="analytics">
+            VISITOR <br /> ANALYTICS <br />{" "}
+            <img src="./images/analytics.png" alt="" />
+          </div>
         </Link>
       </section>
       <p className="greeting">Hello Mx. User! Where to today?</p>
       <div className="logout-btn-placement">
-        <Link to="/Hello_VMS_FrontEnd/">
+        <Link className="links" to="/Hello_VMS_FrontEnd/">
           <p className="logout-btn">LOG OUT</p>
         </Link>
       </div>
