9e2f1cd1a188221fb977de418c05055e7b4dee78
obetdelacruz <pazmichaelandrew70@gmail.com>
2023-06-20 20:32:02 +0800

    Added Logout Confirmation Modal

--- src/components/BackLogoutBtn.jsx
+++ src/components/BackLogoutBtn.jsx
@@ -1,30 +1,39 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import "../styles/header.css";
+import LogoutConfirmationModal from "../modals/LogoutConfirmationModal";
 
 function BackLogoutBtn() {
+  const [showLogout, setShowLogout] = useState(false);
   const navigate = useNavigate();
 
-  const handleLogout = () => {
+  /* const handleLogout = () => {
     window.localStorage.clear();
     navigate("/Hello_VMS_FrontEnd/", { replace: true });
     console.log("You have been loggedout");
-  };
+  }; */
   return (
     <div>
-      <button onClick={() => navigate(-1)}>
-        <div className="back-logout-btn back-placement">BACK</div>
-      </button>
-      <button
-        className="back-logout-btn logout-placement"
-        onClick={() => {
-          handleLogout();
-        }} /* to="/Hello_VMS_FrontEnd/" */
-      >
-        <p className="logout-btn">LOG OUT</p>
-      </button>
-      {/*   <Link to="/Hello_VMS_FrontEnd/">
+      {showLogout ? (
+        <LogoutConfirmationModal setShowLogout={setShowLogout} />
+      ) : null}
+      <div>
+        <button onClick={() => navigate(-1)}>
+          <div className="back-logout-btn back-placement">BACK</div>
+        </button>
+        <button
+          className="back-logout-btn logout-placement"
+          onClick={() => {
+            /* handleLogout(); */
+            setShowLogout(true);
+          }} /* to="/Hello_VMS_FrontEnd/" */
+        >
+          <p className="logout-btn">LOG OUT</p>
+        </button>
+        {/*   <Link to="/Hello_VMS_FrontEnd/">
         <div className="back-logout-btn logout-placement">LOG OUT</div>
       </Link> */}
+      </div>
     </div>
   );
 }
---
+++ src/modals/LogoutConfirmationModal.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "../app.css";
+import "../styles/userManagementResp.css";
+import { useNavigate } from "react-router-dom";
+
+function LogoutConfirmationModal({ setShowLogout }) {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    window.localStorage.clear();
+    navigate("/Hello_VMS_FrontEnd/", { replace: true });
+    console.log("You have been loggedout");
+  };
+
+  return (
+    <div className="background-modal">
+      <div className="remove-user-modal ">
+        Are you sure you want to Logout?
+        <br />
+        <div className="modal-button-placement ">
+          <button className="modalButton" onClick={() => setShowLogout(null)}>
+            NO
+          </button>
+          <button
+            className="modalButton"
+            onClick={() => {
+              setShowLogout(null);
+              handleLogout();
+            }}
+          >
+            YES
+          </button>
+        </div>
+      </div>{" "}
+    </div>
+  );
+}
+
+export default LogoutConfirmationModal;
